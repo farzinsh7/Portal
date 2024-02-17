3b5699c38efc24eb93028b417e6bd9fb0a5f4128
farzinsh7 <farzinnater@gmail.com>
2024-02-17 19:42:03 +0330

    Compatible with Django==5

--- EG_portal/settings.py
+++ EG_portal/settings.py
@@ -40,14 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'sorl.thumbnail',
+    'widget_tweaks',
+    'extensions',
+
+    # Custom_apps
     'portal.apps.PortalConfig',
     'account.apps.AccountConfig',
     'products.apps.ProductsConfig',
     'order.apps.OrderConfig',
-    'widget_tweaks',
-    'extensions',
-    'crispy_forms',
 ]
 
 
@@ -147,8 +150,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 AUTH_USER_MODEL = 'account.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_TLS = True
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
