e6ed2a18c33630bf8af295b45f310a26a70f1cbf
farzinsh7 <farzinnater@gmail.com>
2024-10-30 10:58:25 +0330

    Update Project

--- EG_portal/settings.py
+++ EG_portal/settings.py
@@ -24,7 +24,8 @@ LOGIN_URL = 'login'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "123"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
