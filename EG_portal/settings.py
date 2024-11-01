e9562eee6d95c938ddf99fcd35a7034d843ea265
farzinsh7 <farzinnater@gmail.com>
2024-11-01 13:54:41 +0330

    add new products

--- EG_portal/settings.py
+++ EG_portal/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Tehran'
 
